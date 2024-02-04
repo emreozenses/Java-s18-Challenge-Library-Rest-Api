af5511bb13ab01bb52c210f1fe62f7c2bef6c96b
Emre OZENSES <emreozenses@gmail.com>
2024-02-04 14:13:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java-s18-Challenge
+ JAVA Spring Boot-REST API-Advance Relations of Hibernate
---
+++ src/test/java/com/workintech/Javas18Challenge/JavaS18ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.Javas18Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaS18ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
